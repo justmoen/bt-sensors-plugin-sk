b94d048241751c1fdbbf831b0f78a61e223c34c4
justinmoentrismax <jmoen@trismax.com>
2026-02-22 21:02:26 -0600

    buffer

--- sensor_classes/Ultramax.js
+++ sensor_classes/Ultramax.js
@@ -231,7 +231,8 @@ class Ultramax extends BTSensor {
   async getAndEmitBatteryData() {
     return this.getBuffer(this.buildPollCommand()).then((result) => {
       const buf = Buffer.from(result.toString().replace(/\s+/g, ''), 'hex');
-      const ascii = buf.toString('ascii');
+      console.log("string:", buf);
+      const ascii = buf.toString();
       console.log("string:", ascii);
       const asciiResult = Buffer.from(ascii.substring(25), 'hex');
       console.log("buffer:", asciiResult);
